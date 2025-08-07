24c232fe8e3acb02ff8d8e3b59f208b56176da52
Alasdair Cooper <alasdair.code@hotmail.com>
2025-08-07 12:34:30 +0100

    add useful default parameters

---
+++ src/components/DefaultComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AlasdairCooper.Reference.Components;
+
+public class DefaultComponentBase : ComponentBase
+{
+    [Parameter]
+    public string? Id { get; set; }
+    
+    [Parameter]
+    public string? Class { get; set; }
+    
+    [Parameter]
+    public string? Style { get; set; }
+    
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object?>? AdditionalAttributes { get; set; }
+}
