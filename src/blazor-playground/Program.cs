9ccc94a939ed2d9b91b40cb19d25dc162e93158e
Alasdair Cooper <alasdair.code@hotmail.com>
2025-08-07 16:10:34 +0100

    add button component and basic theming

--- src/blazor-playground/Program.cs
+++ src/blazor-playground/Program.cs
@@ -1,11 +1,13 @@
 using AlasdairCooper.Reference.Components.Dialog;
 using AlasdairCooper.Reference.BlazorPlayground.Components;
+using AlasdairCooper.Reference.Components.Inputs;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents().AddInteractiveServerComponents(x => x.DetailedErrors = true);
 
 builder.Services.AddDialogs();
+builder.Services.AddInputs();
 
 var app = builder.Build();
 
---
+++ src/components/Inputs/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AlasdairCooper.Reference.Components.Inputs;
+
+public static class Extensions
+{
+    public static IServiceCollection AddInputs(this IServiceCollection services)
+    {
+        services.AddBlazoredLocalStorage();
+
+        return services;
+    }
+    
+    public static IServiceCollection AddInputsAsSingleton(this IServiceCollection services)
+    {
+        services.AddBlazoredLocalStorageAsSingleton();
+
+        return services;
+    }
+}
